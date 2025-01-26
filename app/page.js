8e1df314c8abe1a2eec8aef2a3d5468b19c42018
insomnic123 <qaziayn@gmail.com>
2025-01-25 19:25:51 -0500

    Initial Commit: 	deleted:    app/favicon.ico 	new file:   app/fonts/Dongle-Regular.ttf 	new file:   app/fonts/HiMelody-Regular.ttf 	new file:   app/fonts/Inter_24pt-Regular.ttf 	modified:   app/globals.css 	new file:   app/icon.ico 	modified:   app/layout.js 	modified:   app/page.js 	new file:   public/Letters/As/5.png 	new file:   public/Letters/As/6.png 	new file:   public/Letters/As/7.png 	new file:   public/Letters/As/8.png 	new file:   public/Letters/Is/13.png 	new file:   public/Letters/Is/14.png 	new file:   public/Letters/Is/15.png 	new file:   public/Letters/Is/16.png 	new file:   public/Letters/Qs/1.png 	new file:   public/Letters/Qs/2.png 	new file:   public/Letters/Qs/3.png 	new file:   public/Letters/Qs/4.png 	new file:   public/Letters/Zs/10.png 	new file:   public/Letters/Zs/11.png 	new file:   public/Letters/Zs/12.png 	new file:   public/Letters/Zs/9.png 	new file:   public/backgroundImage.png

--- app/page.js
+++ app/page.js
@@ -1,101 +1,31 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+const dongleFont = localFont({
+  src: "fonts/Dongle-Regular.ttf"
+})
+
+const interFont = localFont({
+  src: "fonts/Inter_24pt-Regular.ttf"
+})
+
+const himelodyFont = localFont({
+  src: "fonts/HiMelody-Regular.ttf"
+})
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  <div className = "backgroundImage">
+    <div className = "main">
+      <div className = {`Test m-6 ${himelodyFont.className}`}>
+         <h1>Hey! My name is</h1>
+         <div className = "coloumn"> <Image src="/Letters/Qs/1.png" alt="testtest" width="100" height="100"/> </div>
+         <div className = "coloumn"> <Image src="/Letters/As/5.png" alt="test2" width="100" height="100"/></div>
+         <div className = "coloumn"> <Image src="/Letters/Zs/9.png" alt="test3" width="100" height="100"/></div>
+         <div className = "coloumn"> <Image src="/Letters/Is/13.png" alt="test4" width="100" height="100"/></div>
+      </div>
+    </div>
     </div>
   );
 }
